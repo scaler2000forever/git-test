8c8af7b8e4ce2994cc76932a5f0fd90516e1d186
Lee <Lee@linlin.com>
2022-04-26 18:20:13 +0800

    hot-fix commit

--- src/main/java/com/atlinlin/git/GitTest.java
+++ src/main/java/com/atlinlin/git/GitTest.java
@@ -7,5 +7,8 @@ package com.atlinlin.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git2 commit directly!");
+        System.out.println("正常合并");
     }
 }
