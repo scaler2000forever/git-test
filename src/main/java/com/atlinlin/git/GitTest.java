e0ecaa7382bfd1458a4d9f5fab5b084884eef847
Lee <Lee@linlin.com>
2022-04-26 17:56:26 +0800

    first commit

---
+++ src/main/java/com/atlinlin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atlinlin.git;
+
+/**
+ * @ author : LiLin
+ * @ create : 2022-04-26 17:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
